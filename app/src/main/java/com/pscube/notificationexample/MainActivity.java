3320da2c6502af71236faa401af1c0bb651175ad
possible3656 <sj94611@gmail.com>
2020-04-29 21:06:32 +0530

    notification media style

--- app/src/main/java/com/pscube/notificationexample/MainActivity.java
+++ app/src/main/java/com/pscube/notificationexample/MainActivity.java
@@ -7,8 +7,11 @@ import androidx.core.app.NotificationManagerCompat;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.media.session.MediaSessionCompat;
 import android.view.View;
 
 import android.app.Notification;
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private NotificationManagerCompat notificationManager;
     private EditText editTextTitle;
     private EditText editTextMessage;
+    private MediaSessionCompat mediaSessionCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         editTextTitle = findViewById(R.id.edit_text_title);
         editTextMessage = findViewById(R.id.edit_text_message);
+        mediaSessionCompat=new MediaSessionCompat(this,"tag");
     }
 
     public void sendOnChannel1(View v) {
@@ -44,21 +49,23 @@ public class MainActivity extends AppCompatActivity {
         PendingIntent contentIntent = PendingIntent.getActivity(this,
                 0, activityIntent, 0);
 
-        Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
-        broadcastIntent.putExtra("toastMessage", message);
-        PendingIntent actionIntent = PendingIntent.getBroadcast(this,
-                0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        Bitmap largImage = BitmapFactory.decodeResource(getResources(), R.drawable.ic_one);
+
 
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_one)
                 .setContentTitle(title)
                 .setContentText(message)
+                .setLargeIcon(largImage)
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(largImage)
+                        .bigLargeIcon(null))
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                .setColor(Color.BLUE)
                 .setContentIntent(contentIntent)
                 .setOnlyAlertOnce(true)
-                .addAction(R.mipmap.ic_launcher, "Toast", actionIntent)
                 .build();
 
         notificationManager.notify(1, notification);
@@ -68,10 +75,19 @@ public class MainActivity extends AppCompatActivity {
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
 
+        Bitmap profileimage = BitmapFactory.decodeResource(getResources(), R.drawable.profile);
+
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
                 .setSmallIcon(R.drawable.ic_two)
                 .setContentTitle(title)
                 .setContentText(message)
+                .setLargeIcon(profileimage)
+                .addAction(R.drawable.ic_skip_previous_black_24dp,"previous",null)
+                .addAction(R.drawable.ic_play_arrow_black_24dp,"play",null)
+                .addAction(R.drawable.ic_skip_next_black_24dp,"next",null)
+                .setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
+                .setShowActionsInCompactView(0,1,2)
+                .setMediaSession(mediaSessionCompat.getSessionToken()))
                 .setPriority(NotificationCompat.PRIORITY_LOW)
                 .build();
 
